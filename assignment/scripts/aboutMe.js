d761cb83cc9120fe2ac0f777cf1886c3d5076982
Jake Berg <jberg543@outlook.com>
2024-10-02 13:08:16 -0500

    added first/last/full name

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,12 +1,12 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = '';
+let firstName = 'Jake';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+let lastName = 'Berg';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
-
+let fullName = firstName + lastName; 
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
 
